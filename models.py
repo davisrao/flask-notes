f7bb4a955ea8a6dd5a490354c8f1a043ae5eb053
davisrao <davisrao1@gmail.com>
2021-09-21 12:08:40 -0700

    set up initial model for user

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -18,16 +19,21 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
+    username = db.Column(db.String(20),
+                         primary_key=True)
 
-    username = db.Column(db.Text,
+    password = db.Column(db.String(100),
+                         nullable=False)
+
+    email = db.Column(db.String(50),
                          nullable=False,
                          unique=True)
 
-    password = db.Column(db.Text,
-                         nullable=False)
+    first_name = db.Column(db.String(30),
+                        nullable=False)
+    
+    last_name = db.Column(db.String(30),
+                    nullable=False)
 
     # start_register
     @classmethod
